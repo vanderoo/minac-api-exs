2ea22b0e2d83e801333702599dbea515cff866c8
Evander Tokang <vandertokang17@gmail.com>
2023-06-24 15:36:30 +0700

    updata database config

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
 });
 
