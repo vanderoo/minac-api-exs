d0ccd53f173d21dc0dd080941302bb433f9704d5
Evander Tokang <vandertokang17@gmail.com>
2023-06-28 16:04:11 +0700

    Token Service

--- src/services/TokenService.js
+++ src/services/TokenService.js
@@ -1,5 +1,41 @@
+import crypto from "crypto";
+
 class TokenService {
     constructor(server) {
         this.server = server;
+        this.encryptionKey = crypto.createHash('sha256')
+            .update(this.server.env.APP_KEY, 'utf8')
+            .digest();
     }
+
+    encryptRefreshToken(refreshToken){
+        try {
+            const algorithm = 'aes-256-cbc';
+            const iv = crypto.randomBytes(16);
+
+            const cipher = crypto.createCipheriv(algorithm, this.encryptionKey, iv);
+
+            let encryptedRefreshToken = cipher.update(refreshToken, 'utf8', 'hex');
+            encryptedRefreshToken += cipher.final('hex');
+            return {
+                encryptedRefreshToken,
+                iv: iv.toString('hex'),
+            };
+        }catch(err){
+            console.log(err);
+        }
+
+    }
+
+    decryptRefreshToken(encryptedRefreshToken, iv){
+        const algorithm = 'aes-256-cbc';
+        const decipher = crypto.createDecipheriv(algorithm, this.encryptionKey, Buffer.from(iv, 'hex'));
+
+        let decryptedRefreshToken = decipher.update(encryptedRefreshToken, 'hex', 'utf8');
+        decryptedRefreshToken += decipher.final('utf8');
+        return decryptedRefreshToken;
+    }
+
 }
+
+export default TokenService;
